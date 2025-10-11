ea40c33d0e01692ad26738781aeebc3c04f4ebce
riktimmer <rik.timmer@hotmail.com>
2025-10-11 16:48:22 +0200

    Update PeearLogo.tsx

--- app/components/PeearLogo.tsx
+++ app/components/PeearLogo.tsx
@@ -1,4 +1,3 @@
-// app/components/PeearLogo.tsx
 import * as React from "react";
 
 type Props = {
@@ -6,62 +5,71 @@ type Props = {
 };
 
 /**
- * Inline SVG logo van Peear
- * - gebruikt `currentColor`, dus volgt tekstkleur (light: var(--leaf), dark: white)
+ * Verbeterd Peear-logo:
+ *  - Peer-figuur duidelijker (licht asymmetrisch)
+ *  - Netwerk behouden
+ *  - Volgt tekstkleur (light: var(--leaf), dark: white)
  */
 export default function PeearLogo({ className = "h-40 w-40" }: Props) {
   return (
     <svg
-      viewBox="0 0 256 256"
+      viewBox="0 0 260 260"
       role="img"
       aria-label="Peear logo"
       className={className}
       xmlns="http://www.w3.org/2000/svg"
     >
+      {/* Netwerkstructuur */}
       <g
         fill="none"
         stroke="currentColor"
-        strokeWidth={10}
+        strokeWidth={9}
         strokeLinecap="round"
         strokeLinejoin="round"
         opacity={0.95}
       >
-        <path d="M128 230 38 188 24 98 96 38 160 38 232 98 218 188 128 230" />
-        <path d="M128 72v158" opacity={0.5} />
+        <path d="M130 245 40 190 25 95 95 35 165 35 235 95 220 190 130 245Z" />
+        <path d="M130 75v170" opacity={0.5} />
       </g>
+      {/* Verbinding nodes */}
       {[
-        [128, 230],
-        [38, 188],
-        [24, 98],
-        [96, 38],
-        [160, 38],
-        [232, 98],
-        [218, 188],
+        [130, 245],
+        [40, 190],
+        [25, 95],
+        [95, 35],
+        [165, 35],
+        [235, 95],
+        [220, 190],
       ].map(([cx, cy], i) => (
-        <circle key={i} cx={cx} cy={cy} r={11} fill="currentColor" />
+        <circle key={i} cx={cx} cy={cy} r={10} fill="currentColor" />
       ))}
+
+      {/* Peer-vorm in het midden */}
       <path
         fill="currentColor"
         d="
-          M128 84
-          c -18 -22 -2 -38 10 -41
-          c 10 -2 19 2 22 9
-          c 3 6 2 13 -1 18
-          c 20 8 33 28 33 50
-          c 0 31 -25 56 -56 56
-          s -56 -25 -56 -56
-          c 0 -24 16 -45 38 -52
-          c 4 -1 7 -2 10 -4
+          M120 110
+          c -10 -18 0 -32 10 -36
+          c 8 -4 18 -1 22 6
+          c 3 5 3 12 1 16
+          c 25 6 35 28 30 52
+          c -6 27 -30 44 -56 40
+          c -27 -4 -45 -30 -39 -57
+          c 4 -18 15 -31 32 -36
+          Z
         "
       />
+
+      {/* Steeltje en blad */}
       <path
-        d="
-          M154 45
-          c 10 -10 22 -14 35 -12
-          c -4 12 -14 21 -28 26
-          c -6 2 -10 -4 -7 -14
-        "
         fill="currentColor"
+        d="
+          M152 68
+          c 8 -12 20 -16 32 -13
+          c -5 10 -14 17 -26 20
+          c -4 1 -7 -3 -6 -7
+          Z
+        "
       />
     </svg>
   );
